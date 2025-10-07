b02cd6503b1ed2162ec51114575e0e695c253a06
yamini-vatturi <yaminivatturi@gmail.com>
2025-10-07 18:24:27 +0530

    Create dayfive.py

---
+++ dayfive.py
@@ -0,0 +1,27 @@
+i=1
+while i<=5:
+    print(i)
+    i+=1
+    
+i=5
+while i>=0:
+    print(i)
+    i-=1
+
+i=2
+while i<=10:
+    print(i)
+    i+=2
+
+string="python"
+i=0 
+while i<len(string):
+    print(string[i])
+    i+=1 
+
+n=int(input("write any number"))
+i=1 
+while i<=10:
+    print(n*i)
+    i+=1
+
